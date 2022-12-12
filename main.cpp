e63d093cf495bde1940e4ab733533edf6ce18057
PietroMiniati <118467883+PietroMiniati@users.noreply.github.com>
2022-12-12 16:26:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@
 using namespece std;
 
 float x=0;
-  
+
 int main(){
- int d=0; cin >> d; float numero [d];
-  for (int i=0; i<d; i++) {
-    cin >> numero [i];
-  }
-  for (int i=1; i<d; i++) {
-    float n=maggiore (numero [i], numero [i-1]); 
-    if (n>x) {
-      x=n;
+    int d=0; cin >> d; float numero [d];
+    for (int i=0; i<d; i++) {
+        cin >> numero [i];
     }
-  }
-  cout << x;
-  return 0;
+    for (int i=1; i<d; i++) {
+        float n=maggiore (numero [i], numero [i-1]);
+        if (n>x) {
+            x=n;
+        }
+    }
+    cout << x;
+    return 0;
 }
